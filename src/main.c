517d2cbea8547a7d2a10b8379b6849eb10402a34
Kacper <kacper.gajko1@gmail.com>
2024-08-08 20:06:46 +0200

    ship can accelerate forward

--- src/main.c
+++ src/main.c
@@ -3,6 +3,8 @@
 
 typedef struct Ship{
   Vector2 position;
+  float rotation;
+  float acceleration;
 } Ship;
 
 static Ship ship = { 0 };
@@ -13,12 +15,35 @@ const int windowHeight = 720;
 
 void StartGame(){
   ship.position = (Vector2){windowWidth/2,windowHeight/2};
+  ship.rotation = 0;
+  ship.acceleration = 0;
 };
+
+void UpdateGame(){
+
+  //Ship rotation
+  if(IsKeyDown(KEY_RIGHT)){
+    ship.rotation += 0.04f;
+  }
+  if(IsKeyDown(KEY_LEFT)){
+    ship.rotation -= 0.04f;
+  }
+
+  //ship acceleration
+  if(IsKeyDown(KEY_UP)){
+    ship.acceleration += 0.02f;
+  }else if(ship.acceleration > 0 && !IsKeyDown(KEY_UP)){
+    ship.acceleration -= 0.01f;
+  }
+
+  ship.position.y -= ship.acceleration;
+}
 
 void DrawGame(){
 
   BeginDrawing();
     
+    //Background color
     ClearBackground(BLACK);
 
     //Draw ship
@@ -30,7 +55,6 @@ void DrawGame(){
       (Vector2){ship.position.x - 20, ship.position.y + 20},
       (Vector2){ship.position.x, ship.position.y - 25}
     };
-
     DrawLineStrip(lines, 6, WHITE);
 
   EndDrawing();
@@ -44,6 +68,7 @@ int main(){
 
   while(!WindowShouldClose()){
     DrawGame();
+    UpdateGame();
   }
 
   CloseWindow();
