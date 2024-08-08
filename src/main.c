ea2353d4ee68ba7801b5ebf007b4acb5ea3ad936
Kacper <kacper.gajko1@gmail.com>
2024-08-08 17:50:32 +0200

    displaying square on window

--- src/main.c
+++ src/main.c
@@ -1,25 +1,44 @@
 #include <stdio.h>
 #include <raylib.h>
 
+typedef struct Ship{
+  Vector2 position;
+} Ship;
+
+static Ship ship = { 0 };
+
+//Screen size
+const int windowWidth = 800;
+const int windowHeight = 450;
+
+void StartGame(){
+  ship.position = (Vector2){windowWidth/2,windowHeight/2};
+};
+
+void DrawGame(){
+
+  BeginDrawing();
+    
+    ClearBackground(BLACK);
+
+    //Draw ship
+    DrawPolyLines(ship.position, 4, 10, 45, WHITE);
+
+  EndDrawing();
+}
+
 int main(){
-  const int windowWidth = 800;
-  const int windowHeight = 450;
-
   InitWindow(windowWidth, windowHeight, "Asteroids");
-
   SetTargetFPS(160);
   
+  StartGame();
+
   while(!WindowShouldClose()){
-    BeginDrawing();
-      
-      ClearBackground(RAYWHITE);
-
-      DrawText("HUJ", 190, 200, 20, LIGHTGRAY);
-
-    EndDrawing();
+    DrawGame();
   }
 
   CloseWindow();
   
   return 0;
 }
+
