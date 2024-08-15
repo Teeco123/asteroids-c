af678dfc1dc4f5a97df7cb21aa3beb6132fe7eeb
Kacper <kacper.gajko1@gmail.com>
2024-08-15 17:09:01 +0200

    compiling for windows

---
+++ toolchain-win-x86-64.cmake
@@ -0,0 +1,7 @@
+# toolchain-mingw.cmake
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+# Specify the cross compiler
+set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
